8509a3a392c153242deb23d75ea5b7464af1239d
RebeccaLapointe <RebeccaLapointe@INFO-386-2-P01>
2023-05-23 11:58:25 -0400

    Enlever les requirements. Ajuster les docstring.

--- lune/__main__.py
+++ lune/__main__.py
@@ -2,8 +2,10 @@
     Affiche un dessin de la lune, telle que vu aujourd'hui, puis à une date choisie par l'utilisateur, dans le ciel de l'hémisphère nord
 """
 
+
+from datetime import datetime # pour obtenir la date d'aujourd'hui
 from lune.phase import *
-from datetime import datetime
+
 
 def main():
     print('Lune actuelle')
@@ -17,21 +19,22 @@ def main():
 
     valide = True
     while valide:
-        print("-------------------------------------------------------------------")
-        print("Entrez une date pour voir l'état de la lune. Pour quitter, entrez Q")
+        print('-------------------------------------------------------------------')
+        print('Entrez une date pour voir l\'état de la lune. Pour quitter, entrez Q')
 
         try : 
             # Lire les trois nombres
-            jour = int(input("Entrez un jour "))
-            mois = int(input("Entrez un mois "))
-            annee = int(input("Entrez une annee "))
+            jour = int(input('Entrez un jour '))
+            mois = int(input('Entrez un mois '))
+            annee = int(input('Entrez une annee '))
             if est_date_valide(jour, mois, annee):
                 decrire_lune(jour,mois,annee)
                 print()
             else:
-                print("Ceci n'est pas une date")
+                print('Ceci n\'est pas une date.')
         except:
             valide = False
+
 
 if __name__ == "__main__":
     main()
--- lune/phase.py
+++ lune/phase.py
@@ -1,92 +1,89 @@
 """
-Ce fichier contient des fonctions et des constantes pour le calcul et la représentation de la phase de la lune.
+    Ce fichier contient des fonctions et des constantes pour le calcul et la représentation de la phase de la lune.
 
-Auteur : Rébecca Lapointe
-Date : 20 mai 2023
+    Auteur: Rébecca Lapointe
+    Date: 20 mai 2023
 
-Fonctions:
-- age_lune(jour: int, mois: int, annee: int) -> float:
-    Calcule l'âge de la lune en jours à partir d'une date donnée.
+    Fonctions:
+        age_lune(jour: int, mois: int, annee: int) -> float:
+            Calcule l'âge de la lune en jours à partir d'une date donnée.
 
-- est_croissante(age: float) -> bool:
-    Vérifie si la lune est dans sa phase croissante.
+        est_croissante(age: float) -> bool:
+            Vérifie si la lune est dans sa phase croissante.
 
-- dessiner_lune(age: float, hemisphere_nord: bool = True):
-    Dessine une représentation graphique de la lune en fonction de son âge.
+        dessiner_lune(age: float, hemisphere_nord: bool = True):
+            Dessine une représentation graphique de la lune en fonction de son âge.
 
-- est_illuminee(position: float, largeur: int, luminosite: float, droite_eclairee: bool) -> bool:
-    Vérifie si une position donnée dans la représentation graphique de la lune est éclairée.
+        est_illuminee(position: float, largeur: int, luminosite: float, droite_eclairee: bool) -> bool:
+            Vérifie si une position donnée dans la représentation graphique de la lune est éclairée.
 
-- luminosite(age: float) -> float:
-    Calcule la luminosité de la lune en fonction de son âge.
+        luminosite(age: float) -> float:
+            Calcule la luminosité de la lune en fonction de son âge.
 
-- phase(age: float) -> int:
-    Détermine la phase de la lune en fonction de son âge.
+        phase(age: float) -> str:
+            Détermine la phase de la lune en fonction de son âge.
 
-- decrire_lune(jour: int, mois: int, annee: int):
-    Affiche une description de la phase de la lune pour une date donnée.
+        decrire_lune(jour: int, mois: int, annee: int):
+            Affiche une description de la phase de la lune pour une date donnée.
 """
+
+
 from lune.date import *
 from lune.cercle import *
 
-""" Constantes pour la phases de la lune """
-PHASE_NOUVELLE_LUNE = 0
-PHASE_PREMIER_CROISSANT = 1
-PHASE_PREMIER_QUARTIER = 2
-PHASE_GIBBEUSE_CROISSANTE = 3
-PHASE_PLEINE_LUNE = 4
-PHASE_GIBBEUSE_DECROISSANTE = 5
-PHASE_DERNIER_QUARTIER = 6
-PHASE_DERNIER_CROISSANT = 7
 
 """ Durée du cycle lunaire """
 CYCLE_LUNAIRE = 29.53
 
+
 def age_lune(jour: int, mois: int, annee: int) -> float:
     """
-    Calcule l'âge de la lune en jours à partir d'une date donnée.
+    Calcule l'âge de la lune en jours à partir d'une date donnée. L'âge de la lune correspond au nombre de jour 
+    passé depuis la dernière nouvelle lune.
 
-    Parameters:
-        - jour (int): Le jour.
-        - mois (int): Le mois.
-        - annee (int): L'année.
+        Parameters:
+            jour (int): Le jour.
+            mois (int): Le mois.
+            annee (int): L'année.
 
-    Return:
-        float : L'âge de la lune en jours.
+        Return:
+            float : L'âge de la lune en jours.
     """
     date_nouvelle_lune_connue = jour_julien(13, 1, 2021)
     jours_depuis = jour_julien(jour, mois, annee) - date_nouvelle_lune_connue
     return jours_depuis % CYCLE_LUNAIRE
 
+
 def est_croissante(age: float) -> bool:
     """
-    Vérifie si la lune est dans sa phase croissante.
+    Vérifie si la lune est dans sa phase croissante. La lune croit dans la première moitié de son cycle, puis elle décroit.
 
-    Paramètres :
-        - age (float) : L'âge de la lune en jours.
+        Paramètres :
+            age (float) : L'âge de la lune en jours.
 
-    Retour :
-        bool : True si la lune est dans sa phase croissante, False sinon.
+        Retour :
+            bool : True si la lune est dans sa phase croissante, False sinon.
     """
     return age < CYCLE_LUNAIRE / 2
+
 
 def dessiner_lune(age: float, hemisphere_nord: bool = True):
     """
     Dessine une représentation graphique de la lune en fonction de son âge.
 
-    Parameters :
-        - age (float): L'âge de la lune en jours.
-        - hemisphere_nord (bool): Indique si on doit représenter la lune telle que vue dans l'hémisphère nord, ou. Par défaut, True.
+        Parameters :
+            age (float): L'âge de la lune en jours.
+            hemisphere_nord (bool): Indique si on doit représenter la lune telle que vue dans l'hémisphère nord, ou. Par défaut, True.
 
-    Returns :
-        None
+        Returns :
+            None
     """
     TAILLE_DESSIN = 21
     lum = luminosite(age)
     droite_eclairee = est_croissante(age) ^ (not hemisphere_nord)
 
     for i in range(TAILLE_DESSIN + 1):
-        largeur = corde_horizontale(TAILLE_DESSIN, i)
+        largeur = corde_horizontale(TAILLE_DESSIN, i) #Largeur du cercle lunaire à la ligne i
         for j in range(TAILLE_DESSIN + 1):
             if est_dans_cercle(i, j, TAILLE_DESSIN):
                 decalage = (TAILLE_DESSIN - largeur) / 2
@@ -99,33 +96,37 @@ def dessiner_lune(age: float, hemisphere_nord: bool = True):
             print(c, end = '')
         print()
 
+
 def est_illuminee(position:int, largeur:int, luminosite:float, droite_eclairee: bool) -> bool:
     """
-    Vérifie si une position dans la représentation graphique de la lune est éclairée pour une largeur de lune.
+    Vérifie si une position dans la représentation graphique de la lune est éclairée pour une largeur de dessin.
 
-    Parameters:
-        - position (int): La position à vérifier à partir du début de la lune à gauche
-        - largeur (int): La largeur de la lune
-        - luminosite (float): La luminosité de la lune.
-        - droite_eclairee (bool): Indique si la droite ou la gauche de la lune est éclairée
+        Parameters:
+            position (int): La position à vérifier à partir du début de la lune à gauche
+            largeur (int): La largeur de la lune
+            luminosite (float): La luminosité de la lune.
+            droite_eclairee (bool): Indique si la droite ou la gauche de la lune est éclairée
 
-    Returns:
-        bool: True si la position est éclairée, False sinon.
+        Returns:
+            bool: True si la position est éclairée, False sinon.
     """
     if droite_eclairee:
         return (largeur - position) / largeur < luminosite
     else:
         return position / largeur < luminosite
 
+
 def luminosite(age: float) -> float :
     """
     Calcule la luminosité de la lune en fonction de son âge.
+    La luminosité minimale est 0 à la nouvelle lune.
+    La luminosité maximale est 1 à la pleine lune.
 
-    Parameters:
-        - age (float): L'âge de la lune en jours.
+        Parameters:
+            age (float): L'âge de la lune en jours.
 
-    Returns :
-        float: La luminosité de la lune.
+        Returns :
+            float: La luminosité de la lune.
     """
     mi_lune = CYCLE_LUNAIRE / 2
     if est_croissante(age):
@@ -133,58 +134,50 @@ def luminosite(age: float) -> float :
     else:
         return (CYCLE_LUNAIRE - age) / mi_lune
 
-def phase(age: float) -> int:
+
+def phase(age: float) -> str:
     """
     Détermine la phase de la lune en fonction de son âge.
 
-    Parameters:
-        - age (float): L'âge de la lune en jours.
+        Parameters:
+            age (float): L'âge de la lune en jours.
 
-    Returns:
-        int: La phase de la lune (une valeur parmi les constantes PHASE_*)
+        Returns:
+            str: le nom de la phase de la lune
     """
     l = luminosite(age)
     croissante = est_croissante(age)
     
     if l < 0.04:
-        p = PHASE_NOUVELLE_LUNE
+        p = 'nouvelle lune'
     elif l < 0.35:
-        p = PHASE_PREMIER_CROISSANT if croissante else PHASE_DERNIER_CROISSANT
+        p = 'premier croissant' if croissante else 'dernier croissant'
     elif l < 0.66:
-        p = PHASE_PREMIER_QUARTIER if croissante else PHASE_DERNIER_QUARTIER
+        p = 'premier quartier' if croissante else 'dernier quartier'
     elif l < 0.96:
-        p = PHASE_GIBBEUSE_CROISSANTE if croissante else PHASE_GIBBEUSE_DECROISSANTE
+        p = 'gibbeuse croissante' if croissante else 'gibbeuse décroissante'
     else:
-        p = PHASE_PLEINE_LUNE 
+        p = 'pleine lune' 
     return p
+
 
 def decrire_lune(jour: int, mois: int, annee: int):
     """
     Affiche une description de la phase de la lune pour une date donnée.
 
-    Parameters:
-        - jour (int): Le jour du mois.
-        - mois (int): Le mois.
-        - annee (int): L'année.
+        Parameters:
+            jour (int): Le jour du mois.
+            mois (int): Le mois.
+            annee (int): L'année.
 
-    Returns :
-        None
+        Returns :
+            None
     """
     age = age_lune(jour, mois, annee)
-    p = phase(age)
-    description = [
-        "Nouvelle lune",
-        "Premier croissant",
-        "Premier quartier",
-        "Gibbeuse croissante",
-        "Pleine lune",
-        "Gibbeuse décroissante",
-        "Dernier quartier",
-        "Dernier croissant"
-    ][p]
     l = luminosite(age)
-    print(f"En date du {jour}/{mois}/{annee}, à minuit heure locale.")
-    print(f"La lune a {round(age)} jour(s).")
-    print(f"Elle est dans sa phase {description}. ({round(l * 100)}%)")
+    p = phase(age)
+    print(f'En date du {jour}/{mois}/{annee}, à minuit heure locale.')
+    print(f'La lune a {round(age)} jour(s).')
+    print(f'Elle est dans sa phase "{p}". ({round(l * 100)}% illuminée)')
     dessiner_lune(age)
     print()
